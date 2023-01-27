5990ac1ee908709718a8b0d68092d6b172edb2c3
nonso_o123 <cgobidike@gmail.com>
2023-01-27 14:20:47 -0500

    updated leadership images

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,7 +3,7 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent'
 import { baseUrl } from '../shared/baseUrl'
-import { FadeTransform, Fade, Stagger } from 'react-animation-components'
+import { Fade, Stagger } from 'react-animation-components'
 
 
 function About({ leaders }) {
